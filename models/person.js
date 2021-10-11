64ee7b3d95bd8b28fa616b60068fd1a8701a7c26
Mxx1029 <lia.pim@icloud.com>
2021-10-11 17:02:45 +0200

    Add get user and delete user endpoint

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Score } from './score.js';
 
 const { Schema } = mongoose;
 const required = true;
@@ -6,6 +7,18 @@ const personSchema = new Schema({
     name: { required, type: String },
     scores: [{ type: Schema.Types.ObjectId, ref: "scores" }]
 });
+
+personSchema.pre("remove", (next)=> {
+    console.log("Before removing person ", this);
+    next();
+})
+
+personSchema.post("remove", async (doc, next) => {
+    console.log("After removing person", doc);
+    await Score.deleteMany({ person: doc._id });
+    next();
+})
+
 const Person = mongoose.model("persons", personSchema);
 
 export { personSchema, Person };
